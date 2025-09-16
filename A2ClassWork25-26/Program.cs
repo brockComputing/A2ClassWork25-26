f5c52516f53e2402b2ac9371d1b9a51406feed7e
Charlie Brown <CBROWN@brock.ac.uk>
2025-09-16 09:29:25 +0100

    Add project files.

---
+++ A2ClassWork25-26/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2ClassWork25_26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
