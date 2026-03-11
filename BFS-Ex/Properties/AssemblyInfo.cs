942b953cf936c1aeb00bbd14bece520e4f028896
cbrown <cbrown@brock.ac.uk>
2026-03-11 14:57:55 +0000

    added skeletons

---
+++ BFS-Ex/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BFS-Ex")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Brockenhurst College")]
+[assembly: AssemblyProduct("BFS-Ex")]
+[assembly: AssemblyCopyright("Copyright © Brockenhurst College 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("928c4eaf-6b4c-480c-a5b9-eb1045d4c737")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Dictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionarys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> aDictionary = new Dictionary<string, int>();
+
+            aDictionary.Add("Spitfire", 8);
+            aDictionary.Add("Hurricane", 8);
+            aDictionary.Add("Typhoon", 12);
+            aDictionary.Add("Mustang", 6);
+            aDictionary.Add("B-17", 22);
+
+
+            Console.WriteLine("Enter the plane");
+            string plane = Console.ReadLine();
+            if (aDictionary.ContainsKey(plane))
+            {
+                Console.WriteLine($"The {plane} has {aDictionary[plane]} ");
+            }
+            else
+            {
+
+                Console.WriteLine($"{plane} does not exist");
+            }
+            Console.ReadLine();
+        }
+    }
+}
